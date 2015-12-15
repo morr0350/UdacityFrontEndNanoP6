fdcee2df0b266d7de9a376088a94f2252bd7c11c
Tony Morrow <tony.morrow@gmail.com>
2015-12-14 22:54:44 -0600

    Added Jasmine spec tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,42 +31,104 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('has a nonempty, defined URL', function() {
+            allFeeds.forEach(function (feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).toBeGreaterThan(0);
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('has a nonempty name defined', function () {
+            allFeeds.forEach(function (feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).toBeGreaterThan(0);
+            });
+        })
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The Menu', function () {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('is hidden by default', function () {
+            var body = document.getElementsByTagName('body')[0];
+            expect(body.className).toBe('menu-hidden');
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('becomes visible when clicked', function () {
+            var body = document.getElementsByTagName('body')[0];
+            var menuIcon = $('.menu-icon-link');
+            menuIcon.trigger('click');
 
+            expect(body.className).not.toBe('menu-hidden');
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function () {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        var entries;
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                done();
+            });
+        });
 
+        it ('should contain entries in feed container after load', function () {
+            entries = $('.feed > a.entry-link > article.entry');
+            expect(entries.length).toBeGreaterThan(0);
+        }, 2000);
+    });
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+    describe('New Feed Selection', function () {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var articles1;
+        var articles2;
+        var content1 = [];
+        var content2 = [];
+
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                articles1 = $('.feed > a.entry-link > article.entry');
+                loadFeed(1, function () {
+                    articles2 = $('.feed > a.entry-link > article.entry');
+                    done();
+                });
+            });
+
+        });
+
+        it('should change content when loadfeed() loads a new feed', function() {
+
+            articles1.toArray().forEach(function (article1) {
+                content1.push(article1.innerText);
+            });
+
+            articles2.toArray().forEach(function (article2) {
+                content2.push(article2.innerText);
+            });
+            expect(content1).not.toEqual(content2);
+        });
+
+    })
 }());
